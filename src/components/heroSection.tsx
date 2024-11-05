b07e9a0d859fcebcc3d8a3a10cb5971edb5a8b90
kristinasamuel <166210993+kristinasamuel@users.noreply.github.com>
2024-11-05 21:04:47 +0500

    Class  Assignment 05

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -4,10 +4,10 @@ export default function HeroSection() {
     <>
     <div className="flex flex-col md:flex-row justify-center items-center ">
       <div className="w-full md:w-1/2 flex flex-col items-center md:items-start md:mt-0 mt-10 ">
-        <h1 className="font-bold text-[32px] text-center md:text-left w-full md:w-[496px] md:text-[40px] ">
+        <h1 className="font-bold text-[32px] text-center md:text-left w-[496px] md:text-[40px] ">
           IMPECCABLE CRAFTSMANSHIP AND FINESSE
         </h1>
-        <p className="font-medium md:text-left  text-center text-xl md:text-3xl text-[#787054] mt-3">
+        <p className="font-medium md:text-left text-center text-xl md:text-3xl text-[#787054] mt-3">
           An example of intricate workmanship and detail, elegant necklaces and
           long and short chains form a part of our desirable collection.
         </p>
@@ -15,7 +15,7 @@ export default function HeroSection() {
           Explore Now
         </button>
       </div>
-      <div className="flex flex-col justify-center items-center p-8 md:p-2 mt-5 md:mt-10 overflow-hidden ">
+      <div className="flex flex-col justify-center items-center p-6 md:p-2 mt-5 md:mt-10  overflow-hidden ">
         <Image
           src={"/images/Hero_image.svg"}
           height={647}
