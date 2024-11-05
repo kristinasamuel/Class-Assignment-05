36d9685d0f68ea4dc6901f4819772f9ca496255a
kristinasamuel <166210993+kristinasamuel@users.noreply.github.com>
2024-11-05 19:39:09 +0500

    class assignment 05

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -3,23 +3,23 @@ export default function HeroSection() {
   return (
     <>
     <div className="flex flex-col md:flex-row justify-center items-center ">
-      <div className="w-1/2 md:mt-0 mt-10 text-center md:text-left">
-        <h1 className="font-bold text-[32px] md:text-[40px] md:w-[496px]">
+      <div className="w-full md:w-1/2 flex flex-col items-center md:items-start md:mt-0 mt-10 ">
+        <h1 className="font-bold text-[32px] text-center md:text-left w-full md:text-[40px] md:w-[496px]">
           IMPECCABLE CRAFTSMANSHIP AND FINESSE
         </h1>
-        <p className="font-medium text-xl md:text-3xl text-[#787054] mt-3">
+        <p className="font-medium md:text-left  text-center text-xl md:text-3xl text-[#787054] mt-3">
           An example of intricate workmanship and detail, elegant necklaces and
           long and short chains form a part of our desirable collection.
         </p>
-        <button className="bg-[#A29875] font-medium text-3xl mt-4 text-white md:w-[288px] p-2 md:p-[10px]">
+        <button className="bg-[#A29875] font-medium text-3xl mt-4 items-center md:text-left text-white md:w-[288px] p-2 md:p-[10px] ">
           Explore Now
         </button>
       </div>
-      <div className="w-full md:w-[462px] mt-5 md:mt-10 overflow-hidden ">
+      <div className="flex flex-col justify-center items-center p-5 md:p-2 w-[400px] md:[462px] mt-5 md:mt-10 overflow-hidden ">
         <Image
           src={"/images/Hero_image.svg"}
-          height={647}
-          width={462}
+          height={462}
+          width={647}
           alt="hero_image"
           className="rounded"
         /> 
